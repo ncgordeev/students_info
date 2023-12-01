f168b8ad947668cb796dc1d600ac3fe149c8753d
ncgordeev <ncgordeev@gmail.com>
2023-12-01 21:57:17 +0300

    added functions which get data from lists stud/prof

--- utils.py
+++ utils.py
@@ -2,25 +2,51 @@ import json
 from config import PROF_DATA, STUD_DATA
 
 
-def load_students(filepath):
+def load_students(filepath: str) -> list:
     """
     Function loading data from students.json
-    :return:
+    :return: list
     """
-    with open(filepath) as file:
+    with open(filepath, encoding="utf-8") as file:
         contents = json.load(file)
         return contents
 
 
-def load_professions(filepath):
+students = load_students(STUD_DATA)
+
+
+def load_professions(filepath: str) -> list:
     """
     Function loading data from professions.json
-    :return:
+    :return: list
     """
-    with open(filepath) as file:
+    with open(filepath, encoding="utf-8") as file:
         contents = json.load(file)
         return contents
 
 
-print(load_students(STUD_DATA))
-print(load_professions(PROF_DATA))
+professions = load_professions(PROF_DATA)
+
+
+def get_student_by_pk(students_info) -> dict:
+    """
+    Function getting data by pk
+    :return: dict
+    """
+    for student in students_info:
+        if student["pk"] == 1:
+            return student
+
+
+def get_professions_by_title(professions_info) -> dict:
+    """
+    Function getting data by title
+    :return: dict
+    """
+    for profession in professions_info:
+        if profession["title"] == "Backend":
+            return profession
+
+
+print(get_student_by_pk(students))
+print(get_professions_by_title(professions))
